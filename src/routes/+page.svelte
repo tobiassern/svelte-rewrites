939e2aac3eee1d16c3a6f2dfa28fcabc144a6a96
tobias-hemsol <122261770+tobias-hemsol@users.noreply.github.com>
2023-02-19 12:35:06 +0100

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,5 @@
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+----
+<p>Visit <a href="/test">/test</a> and the rewrite will show you /_target</p>
